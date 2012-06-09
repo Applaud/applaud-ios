a8ba0b4444f4f15355b6a7a8a31fab07f97afa1a
Luke Lovett <fractalbeach@gmail.com>
2012-06-08 22:45:50 -0400

    Initial Commit

---
+++ ApplaudLocations/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ApplaudLocations
+//
+//  Created by Luke Lovett on 6/8/12.
+//  Copyright (c) 2012 Oberlin College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ApplaudLocations/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ApplaudLocations
+//
+//  Created by Luke Lovett on 6/8/12.
+//  Copyright (c) 2012 Oberlin College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
