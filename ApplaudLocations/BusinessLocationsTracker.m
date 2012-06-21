1ff4fdd2f9f47195714ad3ffde691a841e7b1a5a
peter-fogg <peter.fogg@oberlin.edu>
2012-06-20 17:22:26 -0700

    commented out unused variables

--- ApplaudLocations/BusinessLocationsTracker.m
+++ ApplaudLocations/BusinessLocationsTracker.m
@@ -81,11 +81,11 @@
         [[NSNotificationCenter defaultCenter] postNotificationName:@"BUSINESS_RECEIVED" object:businessArray]; 
     };
 
-    NSArray *keyArray = [[NSArray alloc] initWithObjects:@"latitude", @"longitude", nil];
+/*    NSArray *keyArray = [[NSArray alloc] initWithObjects:@"latitude", @"longitude", nil];
     NSArray *valArray = [[NSArray alloc] initWithObjects:
                          [NSNumber numberWithFloat:location.latitude], 
-                         [NSNumber numberWithFloat:location.longitude], nil];
-    NSDictionary *getDict = [[NSDictionary alloc] initWithObjects:valArray forKeys:keyArray];
+                         [NSNumber numberWithFloat:location.longitude], nil];*/
+//    NSDictionary *getDict = [[NSDictionary alloc] initWithObjects:valArray forKeys:keyArray];
 
     // dummy businesses for debugging
     [ConnectionManager serverRequest:@"GET" withParams:nil url:@"/example3/" callback:callback];
