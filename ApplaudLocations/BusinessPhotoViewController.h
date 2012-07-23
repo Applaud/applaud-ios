a94ced205ab5c964a121b4ee24363faaee64edd3
peter-fogg <peter.fogg@oberlin.edu>
2012-07-23 18:13:26 -0400

    added BusinessPhotoViewController files

---
+++ ApplaudLocations/BusinessPhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  BusinessPhotoViewController.h
+//  ApplaudIOS
+//
+//  Created by Peter Fogg on 7/23/12.
+//  Copyright (c) 2012 Applaud, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BusinessPhotoViewController : UIViewController
+
+@end
