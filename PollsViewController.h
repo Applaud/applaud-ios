c6b398dc390c0766881e0049bdb76ab4aa33beb7
Luke Lovett <fractalbeach@gmail.com>
2012-08-06 12:42:29 -0700

    Started adding a polls viewcontroller.

---
+++ PollsViewController.h
@@ -0,0 +1,15 @@
+//
+//  PollsViewController.h
+//  ApplaudIOS
+//
+//  Created by Luke Lovett on 8/6/12.
+//  Copyright (c) 2012 Applaud, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PollsViewController : UIViewController
+
+@property (nonatomic, strong) IBOutlet UITableView *tableView;
+
+@end
---
+++ PollsViewController.m
@@ -0,0 +1,44 @@
+//
+//  PollsViewController.m
+//  ApplaudIOS
+//
+//  Created by Luke Lovett on 8/6/12.
+//  Copyright (c) 2012 Applaud, Inc. All rights reserved.
+//
+
+#import "PollsViewController.h"
+
+@interface PollsViewController ()
+
+@end
+
+@implementation PollsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
